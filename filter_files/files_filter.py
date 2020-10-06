eb292b353a4ccfb1423d785ed388879139c5c70b
GokulanCv <nsew1999gokulcvan@gmail.com>
2020-10-06 22:59:17 +0530

    Update files_filter.py

--- filter_files/files_filter.py
+++ filter_files/files_filter.py
@@ -1,21 +1,3 @@
-import os
-
-"""
-d = "D:/Courses/The Complete Networking Fundamentals Course/2. Basic Networking Terms"
-
-# Return all files in dir, and all its subdirectories, ending in pattern
-def gen_files(dir, pattern):
-   for dirname, subdirs, files in os.walk(dir):
-      for f in files:
-         if f.endswith(pattern):
-            yield os.path.join(dirname, f)
-
-
-# Remove all files in the current dir matching *.config
-pattern = ('.srt', '.vtt')
-for f in gen_files(d, pattern):
-   os.remove(f)
-"""
 
 import os
 class Files:
@@ -35,5 +17,5 @@ class Files:
         for f in self.gen_files(self.dir, self.pattern):
             os.remove(f)
 
-obj = Files("D:/Courses/The Complete Networking Fundamentals Course/3. OSI Model", (".srt", ".vtt"))
+obj = Files(copy the dir path and replace all '/' to '\' , (file types with comma separated))
 obj.remove_files()
